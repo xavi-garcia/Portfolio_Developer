3f317bf070ded2b4acfe70bf29f8f5ba0c3b9d4f
Javier Garcia <trad.ljgarcia@gmail.com>
2022-12-24 23:56:53 -0300

    fixed

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-const videos = gsap.utils.toArray(".video");
-gsap.set(videos, { opacity: 0 });
-
-videos.forEach((video) => {
-  ScrollTrigger.create({
-    trigger: video,
-    start: "top center",
-    end: "bottom center",
-
-    onEnter: () => {
-      gsap.to(video, { opacity: 1 });
-      video.play();
-    },
-    onEnterBack: () => video.play(),
-    onLeave: () => video.pause(),
-    onLeaveBack: () => video.pause(),
-  });
-});
-
-
 const burger = document.querySelector("nav svg");
 
 burger.addEventListener("click", () => {
@@ -38,3 +18,23 @@ burger.addEventListener("click", () => {
   }
   burger.classList.toggle("active");
 });
+
+const videos = gsap.utils.toArray(".video");
+gsap.set(videos, { opacity: 0 });
+
+videos.forEach((video) => {
+  ScrollTrigger.create({
+    trigger: video,
+    start: "top center",
+    end: "bottom center",
+
+    onEnter: () => {
+      gsap.to(video, { opacity: 1 });
+      video.play();
+    },
+    onEnterBack: () => video.play(),
+    onLeave: () => video.pause(),
+    onLeaveBack: () => video.pause(),
+  });
+});
+
